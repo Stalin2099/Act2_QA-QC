7d82be14462486d3023632805b1101f060911a3a
Stalin2099 <stalinnajera416@gmail.com>
2025-12-06 11:06:01 -0500

    Subiendo los archivos incluyendo: integracion

---
+++ integracion.py
@@ -0,0 +1,10 @@
+from calculadora import Calculadora
+
+def flujo_completo():
+    calc = Calculadora()
+    total = calc.suma(10, 5) # Da 15
+    resultado = calc.divide(total, 3) # 15 / 3 = 5.0
+    return resultado
+
+if __name__ == "__main__":
+    print("Flujo integración OK:", flujo_completo()) 
